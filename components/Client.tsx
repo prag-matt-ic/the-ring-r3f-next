6c28cce11a09ad8e47c8f3856d0659cd50035f40
Matthew <matthew@loopspeed.co.uk>
2025-03-23 21:36:53 +0000

    Add particles, name input, initial video placeholder

---
+++ components/Client.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, extend, type ThreeToJSXElements } from "@react-three/fiber";
+import { type FC, useLayoutEffect, useState } from "react";
+import WebGPU from "three/examples/jsm/capabilities/WebGPU.js";
+// import { TheRingPostProcessing } from "@/components/advanced/theRing/TheRingPostProcessing";
+import { type WebGPURendererParameters } from "three/src/renderers/webgpu/WebGPURenderer.js";
+import { WebGPURenderer } from "three/webgpu";
+import * as THREE from "three/webgpu";
+
+import { TheRingNameInput } from "@/components/NameInput";
+import NotSupported from "@/components/NotSupported";
+import {
+  backgroundNode,
+  TheRingParticles,
+  TheRingVideo,
+} from "@/components/TheRingScene";
+
+declare module "@react-three/fiber" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface ThreeElements extends ThreeToJSXElements<typeof THREE> {}
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+extend(THREE as any);
+
+type Props = {
+  name: string | undefined;
+};
+
+const TheRing: FC<Props> = ({ name }) => {
+  const [isSupported, setIsSupported] = useState<boolean | null>(null);
+  const [textPoints, setTextPoints] = useState<Float32Array | null>(null);
+
+  useLayoutEffect(() => {
+    setIsSupported(WebGPU.isAvailable());
+  }, []);
+
+  if (isSupported === null) return null;
+  if (!isSupported) return <NotSupported />;
+
+  return (
+    <>
+      <Canvas
+        className="!fixed inset-0 !h-lvh"
+        performance={{ min: 0.5, debounce: 300 }}
+        scene={{ backgroundNode: backgroundNode }}
+        camera={{ position: [0, 0, 5], far: 10, fov: 70 }}
+        flat={true}
+        gl={async (props) => {
+          const renderer = new WebGPURenderer(
+            props as WebGPURendererParameters
+          );
+          await renderer.init();
+          return renderer;
+        }}
+      >
+        <OrbitControls enableZoom={false} />
+        <TheRingVideo shouldPlay={!!textPoints} />
+        <TheRingParticles textPoints={textPoints} />
+      </Canvas>
+      <TheRingNameInput name={name} setTextPoints={setTextPoints} />
+    </>
+  );
+};
+
+export default TheRing;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
+const config = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "error",
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
