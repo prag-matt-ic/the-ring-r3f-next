546b6bc24faa54c5bb8617aff922e65938e4e30e
Matthew <matthew@loopspeed.co.uk>
2025-03-27 21:26:27 +0000

    Update Not Supported styles

--- components/NotSupported.tsx
+++ components/NotSupported.tsx
@@ -2,18 +2,18 @@ import React, { type FC } from "react";
 
 const NotSupported: FC = () => {
   return (
-    <section className="flex h-svh text-black flex-col items-center justify-center space-y-4 p-8 text-center">
-      <h2 className="text-2xl font-medium tracking-tight">
+    <section className="flex h-svh  flex-col items-center justify-center space-y-4 p-8 text-center">
+      <h2 className="text-5xl font-medium tracking-tight">
         Not supported by this browser
       </h2>
-      <p className="leading-loose text-blue">
+      <p className="text-light leading-loose text-blue text-4xl">
         This experiences uses an experimental technology (WebGPU).
         <br />
-        Please open it using a{" "}
+        Open it using a{" "}
         <a
           target="_blank"
           rel="noreferrer"
-          className="underline"
+          className="underline text-white"
           href="https://developer.mozilla.org/en-US/docs/Web/API/WebGPU_API#browser_compatibility"
         >
           compatible browser
